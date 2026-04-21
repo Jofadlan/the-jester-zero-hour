375dbbfeda9917a6ce14c0198e468dc5f50c1573
nbehaqi-coder <nbehaqi@gmail.com>
2026-04-21 22:08:50 +0700

    bikin map,gerak char,ubah code player

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,47 +1,39 @@
-class_name Player
 extends CharacterBody2D
 
-const SPEED = 200.0
-const INTERACTION_RANGE = 80.0
+const KECEPATAN = 130.0
+var state: State
+var states = {}
+var vel = Vector2.ZERO
+var arah_terakhir = Vector2.RIGHT
 
-@onready var sprite: ColorRect = $Sprite
+func _ready():
+	# load states
+	states["idle"] = load("res://scripts/IdleState.gd").new()
+	states["jalan"] = load("res://scripts/JalanState.gd").new()
+	#states["serang"] = load("res://states/SerangState.gd").new()
 
-var nearest_npc = null
+	# kasih referensi player ke setiap state
+	for s in states.values():
+		s.player = self
 
-func _physics_process(_delta):
-	var direction = 0
-	if Input.is_action_pressed("ui_left"):
-		direction = -1
-		sprite.scale.x = -1
-	elif Input.is_action_pressed("ui_right"):
-		direction = 1
-		sprite.scale.x = 1
+	change_state("idle")
 	
-	velocity.x = direction * SPEED
-	velocity.y = 0
+func get_animasi():
+	return $AnimatedSprite2D
+	
+func change_state(new_state):
+	if state:
+		state.exit()
+	state = states[new_state]
+	state.enter()
+
+func _input(event):
+	state.handle_input(event)
+
+func _process(delta):
+	state.update(delta)
+
+func _physics_process(delta):
+	state.physics_update(delta)
+	velocity = vel
 	move_and_slide()
-	
-	_check_nearest_npc()
-
-func _check_nearest_npc():
-	var npcs = get_tree().get_nodes_in_group("npc")
-	var closest = null
-	var closest_dist = INTERACTION_RANGE
-	
-	for npc in npcs:
-		var dist = global_position.distance_to(npc.global_position)
-		if dist < closest_dist:
-			closest_dist = dist
-			closest = npc
-	
-	# Update indicator
-	if closest != nearest_npc:
-		if nearest_npc != null:
-			nearest_npc.hide_indicator()
-		nearest_npc = closest
-		if nearest_npc != null:
-			nearest_npc.show_indicator()
-
-func _unhandled_input(event):
-	if event.is_action_pressed("interact") and nearest_npc != null:
-		nearest_npc.interact()
